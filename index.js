fa162ddd8c1347c833f8ba83ef47ee472435de82
Tijmen-Scherp <t.kivit@scherponline.nl>
2025-08-20 15:58:56 +0200

    WIP Routes

--- index.js
+++ index.js
@@ -13,33 +13,33 @@ const PRIVATE_APP_ACCESS = process.env.ACCESS_KEY;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+        const snacks = 'https://api.hubspot.com/crm/v3/objects/p_snacks?properties=name,heerlijkheidsscore,pittigheidsgraad';
+        const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(snacks, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Homepage', data });
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-*/ 
-
-app.get('/', async (req, res) => {
-    
-
-});
 app.get('/update-cobj', async (req, res) => {
       
        const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
+
+
     try {
-      
         res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
     } catch (error) {
         console.error(error);
@@ -47,51 +47,41 @@ app.get('/update-cobj', async (req, res) => {
 
 
 });
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+
 app.post('/update-cobj', async (req, res) => {
 
-});
+    const { name, pittigheidsgraad, heerlijkheidsscore } = req.body;
 
+    const properties = {};
+    if (name) properties.name = name;
+    if (pittigheidsgraad) properties.pittigheidsgraad = pittigheidsgraad;
+    if (heerlijkheidsscore) properties.heerlijkheidsscore = heerlijkheidsscore;
 
+    const url = `https://api.hubspot.com/crm/v3/objects/p_snacks/${name}`;
 
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+        await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ properties })
+        });
+
+        res.send('<p>Snack record updated!</p><a href="/update-cobj">Update another</a>');
     } catch (error) {
         console.error(error);
+        res.status(500).send(`<p>Error updating snack: ${error.message}</p>`);
     }
 });
 
+/** 
+* * This is sample code to give you a reference for how you should structure your calls. 
 
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-
+*/ 
 
 
 // * Localhost
